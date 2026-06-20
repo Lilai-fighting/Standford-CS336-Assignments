44a9c318dde91d56dc33ffc732935d7f25d8e7ea
Rock <1954301037@qq.com>
2026-06-20 17:40:28 +0100

    split on special tokens before pre-tokenization

--- assignment1-basics/cs336_basics/train_bpe.py
+++ assignment1-basics/cs336_basics/train_bpe.py
@@ -8,9 +8,13 @@ def pre_tokenization(text: str, special_tokens: list[str]):
     Implement a regex-based pre-tokenizer and apply it on the corpus.
     """
     freq_table: dict[tuple[bytes, ...], int] = {}
+    # split text on special tokens, 
+    # use escape to avoid splitting text on partial special tokens
+    delimiter = "|".join(re.escape(token) for token in special_tokens)
+    text_split = re.split(delimiter, text)
 
     # avoid storing pre_tokenized words
-    for match in re.finditer(PAT, text):
+    for match in re.finditer(PAT, text_split):
         pre_token = match.group() # split is a match object, not string
         # iterating over encoded string gives integers
         pre_token_bytes = tuple(bytes([s]) for s in pre_token.encode("utf-8"))
@@ -53,26 +57,26 @@ def merge_once(freq_table: dict[tuple[bytes, ...], int], target_merge: tuple[byt
 
     return new_freq_table
 
-def merge(freq_table: dict[tuple[bytes, ...], int], vocabulary: dict[int, bytes], end_id:int, times: int):
+def merge(freq_table: dict[tuple[bytes, ...], int], vocabulary: dict[int, bytes], end_id: int, vocab_size: int):
     """
     Compute the BPE merges (i.e., train the BPE tokenizer).
     """
     merges: list[tuple[bytes, bytes]] = []
     new_freq_table = freq_table
-    new_vocab = vocabulary
+    merge_times = vocab_size - len(vocabulary)
 
-    for _ in range(times):
+    for _ in range(merge_times):
         pairs_table = count_pairs_in_words(new_freq_table)
         # first find the most frequent fair; choose the lexicographically greatest pair if tied
         most_common_pair = max(pairs_table, key=lambda pair: (pairs_table[pair], pair))
         new_freq_table = merge_once(new_freq_table, most_common_pair)
         merges.append(most_common_pair)
-        new_vocab[end_id] = most_common_pair[0] + most_common_pair[1]
+        vocabulary[end_id] = most_common_pair[0] + most_common_pair[1]
         end_id += 1
 
-    return new_vocab, merges
+    return vocabulary, merges
 
-def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str], times:int):
+def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
     """
     Training a (byte-level) BPE tokenizer.
     """
@@ -89,19 +93,6 @@ def train_bpe(input_path: str, vocab_size: int, special_tokens: list[str], times
     freq_table = pre_tokenization(text, special_tokens)
 
     # Compute BPE merges (i.e., Train BPE tokenizer)
-    new_vocab, merges = merge(freq_table, vocab, end_id, times)
+    new_vocab, merges = merge(freq_table, vocab, end_id, vocab_size)
 
     return new_vocab, merges
-
-if __name__ == "__main__":
-    input_path = Path(__file__).parents[1] / "tests" / "bpe_example.txt"
-    vocab, merges = train_bpe(
-        input_path,
-        vocab_size=300,
-        special_tokens=["<|endoftext|>"],
-        times=6
-    )
-    print(merges)
-    print("------------------")
-    print(len(vocab))
-    print(list(vocab.items())[-6:])
